c8ffb0cda1f9174c1a668119789d78071aeb1080
AbuBakar <muhammadabubakar85033@gmail.com>
2026-06-07 15:27:34 +0500

    removed useless/doubled folders

--- OnlineQuizSystem/obj/Release/net10.0/OnlineQuizSystem.AssemblyInfo.cs
+++ OnlineQuizSystem/obj/Release/net10.0/OnlineQuizSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineQuizSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+049c17d78213ecaefbca092d932ec877f213f09c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineQuizSystem")]
